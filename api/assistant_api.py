21dab2aa65bc543a6df34de17159babd1f3f5295
Jan Banot <banot.jan@gmail.com>
2024-03-01 21:36:49 +0100

    feat: basic project structure and flask api

---
+++ api/assistant_api.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+
+load_dotenv()
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if (request.method == 'GET'):
+        data = "hello world"
+        app.logger.info('Request: %s', request)
+        app.logger.info('Response: %s', data)
+        return jsonify({'data': data})
+
+
+if __name__ == '__main__':
+    debug_mode = os.getenv('FLASK_DEBUG_MODE', 'False')
+    app.run(debug=debug_mode.lower() == 'true')
