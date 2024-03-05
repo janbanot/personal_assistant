645899b91f51730a353cf9cf22ab32db96e1502d
Jan Banot <banot.jan@gmail.com>
2024-03-06 00:40:27 +0100

    fix: build script
    fix build script to correctly handle --local flag

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 docker build --no-cache -t assistant_api .
 docker-compose build
 
-if [ "$1" == "local" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose.override.yml up
+if [[ $@ == *"--local"* ]]; then
+  docker-compose -f docker-compose.yml -f docker-compose.override.yml up --build
 else
-    docker-compose -f docker-compose.yml up
+  docker-compose up --build
 fi
