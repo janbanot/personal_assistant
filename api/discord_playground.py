5738cefa18e411cd2e8beb3c0d5a0a499636b499
Jan Banot <banot.jan@gmail.com>
2024-03-15 23:15:55 +0100

    feat: discord bot foundation

---
+++ api/discord_playground.py
@@ -0,0 +1,29 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
