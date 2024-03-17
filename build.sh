2eec864b738efeb8230b1cf1a28a4427bf2982d0
Jan Banot <banot.jan@gmail.com>
2024-03-17 13:11:05 +0100

    feat: add continer status check to the script

--- build.sh
+++ build.sh
@@ -6,4 +6,15 @@ if [[ $1 == "--local" ]]; then
   docker-compose -f docker-compose.yml -f docker-compose.override.yml up --build
 else
   docker-compose up -d --build
+
+  # Wait for a short period of time for the containers to start up
+  sleep 30
+
+  # Check the status of the containers
+  for container in $(docker-compose ps -q); do
+    if [ "$(docker inspect -f '{{.State.Running}}' $container)" != "true" ]; then
+      echo "Container $container is not running correctly"
+      exit 1
+    fi
+  done
 fi
