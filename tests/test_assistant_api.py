3d6c1874aa5043e7b39a0016b376b6eca481ed75
Jan Banot <banot.jan@gmail.com>
2024-03-18 22:40:59 +0100

    tmp: add dummy test to check workflow

--- tests/test_assistant_api.py
+++ tests/test_assistant_api.py
@@ -9,12 +9,16 @@ def client():
         yield client
 
 
-def test_home_get(client):
-    response = client.get('/')
-    assert response.status_code == 200
-    assert response.get_json() == {'data': 'hello world'}
+def test_dummy():
+    assert True
 
 
-def test_home_post(client):
-    response = client.post('/')
-    assert response.status_code == 405  # Method Not Allowed
+# def test_home_get(client):
+#     response = client.get('/')
+#     assert response.status_code == 200
+#     assert response.get_json() == {'data': 'hello world'}
+
+
+# def test_home_post(client):
+#     response = client.post('/')
+#     assert response.status_code == 405  # Method Not Allowed
