8d470724faec3b480f2d7a68f7a1fb5c4788b686
Jan Banot <banot.jan@gmail.com>
2024-03-28 21:08:38 +0100

    feat: basic login endpoint tests

--- tests/test_assistant_api.py
+++ tests/test_assistant_api.py
@@ -1,24 +1,37 @@
 import pytest
 from api.assistant_api import app
 
+# from helpers import authenticate_test_client
+
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
 
 
-def test_dummy():
-    assert True
+def login(client, email, password):
+    data = {"email": email, "password": password}
+    headers = {"Content-Type": "application/json"}
+    return client.post("/login", json=data, headers=headers)
 
 
-# def test_home_get(client):
-#     response = client.get('/')
-#     assert response.status_code == 200
-#     assert response.get_json() == {'data': 'hello world'}
+def test_login_route(client):
+    response = login(client, "test@test.com", "test1")
+    assert response.status_code == 200
+    assert response.json["message"] == "Login Successful"
+    assert "access_token" in response.json
+    assert response.json["expires_in"] == 1800
 
 
-# def test_home_post(client):
-#     response = client.post('/')
-#     assert response.status_code == 405  # Method Not Allowed
+def test_login_route_with_incorrect_password(client):
+    response = login(client, "test@test.com", "wrong_password")
+    assert response.status_code == 401
+    assert "access_token" not in response.json
+
+
+def test_login_route_with_incorrect_email(client):
+    response = login(client, "test1@test.com", "test1")
+    assert response.status_code == 401
+    assert "access_token" not in response.json
