c4ae59de6b6113248564cbe488c9a3fb6c79aea4
Jan Banot <banot.jan@gmail.com>
2024-05-01 22:41:36 +0200

    fix: formatting change

--- discord_bot/bot_commands.py
+++ discord_bot/bot_commands.py
@@ -5,18 +5,18 @@ Command = namedtuple("Command", ["name", "description"])
 
 
 class BotCommands(Enum):
-    LIST_COMMANDS = Command("list-commands", "List all available commands")
+    LIST_COMMANDS = Command("list-commands", "list all available commands")
     YT_SUMMARY = Command(
-        "yt-summary", "Get a summary of a YouTube video. Provide a URL"
+        "yt-summary", "get a summary of a YouTube video. Provide a URL"
     )
     CHECK_ENGLISH = Command(
         "check-english",
-        "Check and fix grammatical, spelling, and punctuation errors in English text",
+        "Cceck and fix grammatical, spelling, and punctuation errors in English text",
     )
 
 
 def get_bot_commands():
     commands = []
     for command in BotCommands:
-        commands.append(f"{command.value.name}: {command.value.description}")
+        commands.append(f"- **{command.value.name}**: {command.value.description}")
     return "\n".join(commands)
