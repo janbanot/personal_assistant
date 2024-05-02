4170e27cb56e292ca50db476e0f61858eca9b75c
Jan Banot <banot.jan@gmail.com>
2024-05-02 21:42:51 +0200

    fix: command description

--- discord_bot/bot_commands.py
+++ discord_bot/bot_commands.py
@@ -7,11 +7,11 @@ Command = namedtuple("Command", ["name", "description"])
 class BotCommands(Enum):
     LIST_COMMANDS = Command("list-commands", "list all available commands")
     YT_SUMMARY = Command(
-        "yt-summary", "get a summary of a YouTube video. Provide a URL"
+        "yt-summary", "get a summary of a YouTube video, provide a URL"
     )
     CHECK_ENGLISH = Command(
         "check-english",
-        "Cceck and fix grammatical, spelling, and punctuation errors in English text",
+        "check and fix grammatical, spelling, and punctuation errors in English text",
     )
 
 
