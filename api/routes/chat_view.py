b75a9d6051dab4dea583467832ad1066a243cb97
Jan Banot <banot.jan@gmail.com>
2024-06-02 22:18:36 +0200

    feat: add datetime and improve chat prompt

--- api/routes/chat_view.py
+++ api/routes/chat_view.py
@@ -2,6 +2,7 @@ from flask import request, jsonify
 from flask.views import MethodView
 from flask_jwt_extended import jwt_required
 from dotenv import load_dotenv
+from datetime import datetime
 from langchain_openai import ChatOpenAI
 from langchain.prompts.prompt import PromptTemplate
 from langchain.chains import ConversationChain
@@ -15,9 +16,24 @@ context_memory = ConversationSummaryMemory(llm=ChatOpenAI(), ai_prefix="Assistan
 # TODO: refactor to use the same instance of the model
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
-template = """The following is a conversation between a human and an AI personal assistant.
-The Assistant provide clear and concise answers to the with minimum unnecessary information.
-If the Assistant does not know the answer to a question, it truthfully says it does not know.
+template = """
+You are an AI assistant designed for ultra-concise, engaging conversations.
+Follow these rules:
+- Use the fewest words possible while maintaining clarity, impact and natural language
+- Keep a friendly, casual tone with occasional colloquialisms
+- Format responses in Markdown or JSON, like `**bold**` or `{"key": "value"}`
+- Always wrap code with triple backticks and keywords with `single backticks`
+- Ask for clarification to avoid assumptions
+- Detect intentions and emotional states to tailor responses perfectly.
+- Focus solely on instructions and provide relevant, comprehensive responses
+- Never repeat info or mention limitations
+- Simplify complex tasks; provide the best output possible
+- Prioritize user needs; tailor responses to their context and goals
+- When asked for specific content, start response with requested info immediately
+- Continuously improve based on user feedback
+
+Note:
+- Current Date (YYYY/MM/DD, HH:MM:SS): {current_datetime}
 
 Current conversation:
 {history}
@@ -26,7 +42,7 @@ AI Assistant:
 """
 
 # TODO: Check why langchain default template example is sent in the context with every message?
-PROMPT = PromptTemplate(input_variables=["history", "input"], template=template)
+PROMPT = PromptTemplate(input_variables=["history", "input", "current_datetime"], template=template)
 
 conversation = ConversationChain(
     prompt=PROMPT,
@@ -49,7 +65,9 @@ class ChatView(MethodView):
         # TODO: buffer or context_memory?
         current_context = context_memory.buffer
 
-        result = conversation.predict(input=input_text)
+        current_datetime = datetime.now().strftime("%Y/%m/%d, %H:%M:%S")
+
+        result = conversation.predict(input=input_text, current_datetime=current_datetime)
 
         self.db_manager.save_message(conversation_id, input_text, current_context, result)
 
